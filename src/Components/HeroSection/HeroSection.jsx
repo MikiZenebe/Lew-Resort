78a663bb9ed8bb974d5b037f46a6e4a6dc8150b2
elfe digital <elfesolution1@gmail.com>
2025-06-27 12:00:47 +0300

    sads

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -29,10 +29,7 @@ const HeroSection = () => {
       >
         {/* slider 1 */}
         <SwiperSlide>
-          <div
-            className="bg-[url('/images/home-1/Lewishorttrailer.mp4')] w-full h-[700px] md:h-[800px] xl:h-[850px] 3xl:h-[950px]  bg-[#1e1e1ede] bg-opacity-90 grid items-center bg-cover justify-center text-white relative pb-[150px] lg:pb-16 xl:pb-0 "
-            data-aos="fade-down"
-          >
+      
 
 <div className="relative w-full h-screen overflow-hidden">
   <video
