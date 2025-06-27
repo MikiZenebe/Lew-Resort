a95d5c1930f1aecacb0a18d1440f13c7375599ab
elfe digital <elfesolution1@gmail.com>
2025-06-27 11:53:58 +0300

    sdf

--- src/Pages/InnerPage/RoomDetails.jsx
+++ src/Pages/InnerPage/RoomDetails.jsx
@@ -102,7 +102,7 @@ const RoomDetails = () => {
               >
                 {bookingsData && bookingsData.title
                   ? bookingsData.title
-                  : "Deluxe Wolaita Sodo"}d
+                  : "Hawassa Rooms"}d
               </h2>
               <p
                 className="text-sm lg:text-base leading-6 text-gray dark:text-lightGray font-normal font-Lora"
