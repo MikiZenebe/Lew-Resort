0ba490a6e2e0f0a983cc9b0a702d41b3452a253b
elfe digital <elfesolution1@gmail.com>
2025-06-29 01:10:53 +0300

    aA

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,52 +1,28 @@
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "./style.css";
-
-// import required modules
-import { Autoplay, Navigation, Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { FaStar } from "react-icons/fa";
-import { BiPhoneCall } from "react-icons/bi";
-import { Link } from "react-router-dom";
-
-const HeroSection = () => {
-  return (
-    <div className="">
-      <Swiper
-        centeredSlides={true}
-        navigation={true}
-        speed="3000"
-        autoplay={{
-          delay: 10000,
-          disableOnInteraction: true,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Navigation, Autoplay, Pagination]}
-        className="mySwiper"
-      >
-        {/* slider 1 */}
 <SwiperSlide>
   <div
     className="relative w-full h-[700px] md:h-[800px] xl:h-[850px] 3xl:h-[950px] pb-[150px] lg:pb-16 xl:pb-0 overflow-hidden"
     data-aos="fade-down"
   >
-    {/* video as the background */}
+    {/* 1. VIDEO (No changes needed here) */}
+    {/* This is your background video, positioned to cover the container. */}
     <video
       autoPlay
       muted
       loop
       playsInline
-      className="absolute top-0 left-0 w-full h-full object-cover"
+      className="absolute top-0 left-0 w-full h-full object-cover z-0"
     >
       <source src="/images/home-1/Lewishorttrailer.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
 
-    {/* overlay text */}
-    <div className="relative z-10 grid items-center justify-center w-full h-full bg-[#1e1e1ede] bg-opacity-90 text-white">
+    {/* 2. OVERLAY (This is the new dedicated element) */}
+    {/* This creates the dark tint on top of the video, making text readable. */}
+    <div className="absolute top-0 left-0 w-full h-full bg-black/70 z-10"></div>
+
+    {/* 3. CONTENT (Background color removed, z-index increased) */}
+    {/* This container now only holds your content and sits on top of the overlay. */}
+    <div className="relative z-20 grid items-center justify-center w-full h-full text-white">
       <div className="font-Garamond 2xl:w-[720px] text-center">
         <div className="flex space-x-2 items-center justify-center mb-5 lg:mb-6">
           <FaStar className="w-[14px] h-[14px] lg:w-[16px] lg:h-[16px] text-[#1cc3b2]" />
@@ -78,12 +54,3 @@ const HeroSection = () => {
     </div>
   </div>
 </SwiperSlide>
-
-        
-        {/* slider 2 */}
-        </Swiper>
-    </div>
-  );
-};
-
-export default HeroSection;
