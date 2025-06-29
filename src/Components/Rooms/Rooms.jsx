f84aa5e4cc594fd0770b670b908228ad21069cc8
elfe digital <elfesolution1@gmail.com>
2025-06-30 01:26:31 +0300

    imageupdate

--- src/Components/Rooms/Rooms.jsx
+++ src/Components/Rooms/Rooms.jsx
@@ -231,7 +231,7 @@ const Rooms = () => {
                     <div className="relative">
                       <div className="overflow-hidden">
                         <img
-                          src="/images/welita/1D6A5555.jpg"
+                          src="/images/Bonga/5F3A3639.jpg"
                           className="w-full h-full object-cover group-hover:scale-110 transition-all duration-300"
                           alt=""
                         />
