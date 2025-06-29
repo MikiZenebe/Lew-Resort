da8fabf45df321521406fbeb2e82e8440e6fb87e
elfe digital <elfesolution1@gmail.com>
2025-06-30 01:25:14 +0300

    sadf

--- src/Components/Rooms/Rooms.jsx
+++ src/Components/Rooms/Rooms.jsx
@@ -220,10 +220,90 @@ const Rooms = () => {
           {/* Rooms Slider Container */}
 
 
-        
-
           <div className="relative">
             <div className="mt-14 2xl:mt-[60px] keen-slider " ref={sliderRef}>
+
+
+              {/* slide - 0 new */}
+              <div className="keen-slider__slide number-slide1 ">
+                <div data-aos="fade-up-left" data-aos-duration="1000">
+                  <div className="overflow-x-hidden 3xl:w-[410px] group relative">
+                    <div className="relative">
+                      <div className="overflow-hidden">
+                        <img
+                          src="/images/welita/1D6A5555.jpg"
+                          className="w-full h-full object-cover group-hover:scale-110 transition-all duration-300"
+                          alt=""
+                        />
+                      </div>
+                      <div className="">
+                        <Link to={"/room_details"}>
+                          <button className="flex items-center justify-center text-[15px] leading-[38px] bg-lightBlack absolute bottom-0 -left-40 px-5 text-white  group-hover:left-0 transition-all duration-300 hover:bg-khaki">
+                            View Details{" "}
+                            <BsArrowRight className="w-4 h-4 ml-2  text-white" />{" "}
+                          </button>
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="font-Garamond">
+                      <div className="px-5 3xl:px-6 py-2 inline-flex bg-khaki text-sm  items-center justify-center text-white  absolute top-[10px] right-[10px] font-Lora font-normal leading-[26px]">
+                        <span className="">$560</span>
+                        <span className="mx-2">|</span>
+                        <span>Night</span>
+                      </div>
+
+                      <div className=" border-[1px] border-[#e8e8e8] dark:border-[#424242] border-t-0">
+                        <div className="py-6 px-[30px]">
+                          <h4 className="text-sm leading-[26px] text-khaki uppercase font-semibold">
+                            Luxury Room
+                          </h4>
+                          <Link to="/room">
+                            <h2 className="text-2xl lg:text-[28px] leading-[26px] font-semibold text-lightBlack dark:text-white py-4">
+                              Bonga
+                            </h2>
+                          </Link>
+                          {/* <p className="text-sm font-normal text-gray  dark:text-lightGray font-Lora">
+                            1500 SQ.FT/Rooms
+                          </p> */}
+                        </div>
+                        <div className="  border-t-[1px] border-[#e8e8e8] dark:border-[#424242] py-5">
+                          <div className="px-[30px] flex items-center justify-between">
+                            <div className="">
+                              <span className="font-Lora text-base flex items-center ">
+                                <img
+                                  src="/images/home-1/room-bottom-icon.png"
+                                  alt=""
+                                />
+                                <span className="ml-[10px] text-gray dark:text-lightGray">
+                                  2 King Bed
+                                </span>
+                              </span>
+                            </div>
+                            <span className="w-[1px] h-[25px] bg-[#ddd] dark:bg-gray"></span>
+                            <ul className="flex items-center text-khaki space-x-[5px]">
+                              <li>
+                                <FaStar />
+                              </li>
+                              <li>
+                                <FaStar />
+                              </li>
+                              <li>
+                                <FaStar />
+                              </li>
+                              <li>
+                                <FaStar />
+                              </li>
+                              <li>
+                                <FaStar />
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
               {/* slide - 1 */}
               <div className="keen-slider__slide number-slide1 ">
                 <div data-aos="fade-up-left" data-aos-duration="1000">
